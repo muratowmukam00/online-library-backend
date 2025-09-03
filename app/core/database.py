f8f01d88530ba0911af99c6fbc0aa601448742d6
muratowmukam00 <muratowmukam00@gmail.com>
2025-09-03 16:44:22 +0500

    Setup FastApi skeleton and databse config.

---
+++ app/core/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASS", "password")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "online_library")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# try:
+#     connection = engine.connect()
+#     print("Connection established")
+#     connection.close()
+# except Exception as e:
+#     print("Connection failed")
