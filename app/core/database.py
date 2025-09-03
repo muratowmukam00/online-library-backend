ed7fe94e718b7b448fc48ce80486715fc061c067
muratowmukam00 <muratowmukam00@gmail.com>
2025-09-03 20:13:56 +0500

    Implement User model, schemas and auth setup

--- app/core/database.py
+++ app/core/database.py
@@ -12,7 +12,7 @@ DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "online_library")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
+print("Connecting DB with URL: ", DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.routers import auth
 
 app = FastAPI(title="Online Library Backend")
 
 @app.get("/")
 def read_root():
     return {"msg": "Online  Library API is running"}
+
+app.include_router(auth.router, prefix="/auth")
