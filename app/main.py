69ade6097363dd0d9efd12b0af79cb1dac9912ce
muratowmukam00 <muratowmukam00@gmail.com>
2025-09-07 00:13:02 +0500

    fix: Corrected ResponseValidationError in admin setup router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from app.routers import auth, user
+from app.routers import auth, user, admin_setup
 
 app = FastAPI(title="Online Library Backend")
 
 @app.get("/")
 def read_root():
     return {"msg": "Online  Library API is running"}
+
+app.include_router(admin_setup.router, prefix="/admin-setup" ,tags=["admin_setup"])
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(user.router, prefix="/users", tags=["users"])
